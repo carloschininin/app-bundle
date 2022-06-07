b2bc2f5adafd2cc2474a4ab5e8df3b63c35c5389
Carlos Chininin <carloschininin@gmail.com>
2022-06-07 14:27:35 -0500

    add load extension import services

--- src/CarlosChinininAppBundle.php
+++ src/CarlosChinininAppBundle.php
@@ -9,8 +9,14 @@ declare(strict_types=1);
 
 namespace CarlosChininin\AppBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class CarlosChinininAppBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
